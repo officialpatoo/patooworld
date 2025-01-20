b0a6d196b9504acc3ebea945c56ef4b13ad3d351
PATOOWORLD <favour120012@icloud.com>
2025-01-20 07:21:53 -0800

    Update empty-screen.tsx
    added new one

--- morphic/components/empty-screen.tsx
+++ morphic/components/empty-screen.tsx
@@ -3,16 +3,16 @@ import { ArrowRight } from 'lucide-react'
 
 const exampleMessages = [
   {
-    heading: 'What is OpenAI o1?',
-    message: 'What is OpenAI o1?'
+    heading: 'How do you Assist Users?',
+    message: 'How do you Assist Users?'
   },
   {
     heading: 'Why is Nvidia growing rapidly?',
     message: 'Why is Nvidia growing rapidly?'
   },
   {
-    heading: 'Tesla vs Rivian',
-    message: 'Tesla vs Rivian'
+    heading: 'Latest Technology Trends',
+    message: 'Latest Technology Trends'
   },
   {
     heading: 'Summary: https://arxiv.org/pdf/2407.16833',
