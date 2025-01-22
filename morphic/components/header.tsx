79cc39ff2a46f23b5663eb28de66f2f624ca7db4
PATOOWORLD <favour120012@icloud.com>
2025-01-22 13:49:35 -0800

    Update header.tsx

--- morphic/components/header.tsx
+++ morphic/components/header.tsx
@@ -8,14 +8,10 @@ export const Header: React.FC = async () => {
   return (
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
-       <script src="https://www.gstatic.com/firebasejs/8.0/firebase.js"></script>
-<script>
-  var config = {
-    apiKey: "AIzaSyDjf6PSBi4TJy6uilcpfSEic-_YCzUa38s",
-    authDomain: "elite-flow-386908.firebaseapp.com",
-  };
-  firebase.initializeApp(config);
-</script>
+       <a href="/">
+          <IconLogo className={cn('w-5 h-5')} />
+          <span className="sr-only">Morphic</span>
+        </a>
       </div>
       <div className="flex gap-0.5">
         <ModeToggle />
