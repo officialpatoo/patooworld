2eb9209a7bad2c432935fb97b1ff8cf53127a955
PATOOWORLD <favour120012@icloud.com>
2025-01-23 15:41:16 -0800

    Update models.ts

--- morphic/lib/types/models.ts
+++ morphic/lib/types/models.ts
@@ -9,13 +9,13 @@ export const models: Model[] = [
 {
   id: 'gemini-1.5-pro-002',
   name: 'PATOOWORLD',
-  provider: 'Create human-quality text, poems, codes, and many different kinds of creative content',
+  provider: 'Your Choice, We Adore',
   providerId: 'google'
   },
   {
     id: 'gemini-2.0-flash-exp',
     name: '99PACE',
-     provider: 'Speed and Efficiency: Prioritizes fast response times and low latency, it ideal for TASK',
+     provider: 'Speed and Efficiency',
      providerId: 'google'
     }
 ]
