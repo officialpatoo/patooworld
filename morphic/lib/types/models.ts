b4b54b03075555e9523e695395b2dcca24515f4a
PATOOWORLD <favour120012@icloud.com>
2025-01-23 15:23:53 -0800

    Update models.ts
    added back gemini flash (99pace)

--- morphic/lib/types/models.ts
+++ morphic/lib/types/models.ts
@@ -8,20 +8,14 @@ export interface Model {
 export const models: Model[] = [
 {
   id: 'gemini-1.5-pro-002',
-  name: 'PATOO ASSIST',
-  provider: 'Create human-quality text, write different kinds of creative content',
+  name: 'PATOOWORLD',
+  provider: 'Create human-quality text, poems, codes, and many different kinds of creative content',
   providerId: 'google'
   },
   {
     id: 'gemini-2.0-flash-exp',
-    name: '99PACE AI',
-     provider: 'Speed and Efficiency: Prioritizes fast response times and low latency, making it ideal for time-sensitive and high-frequency tasks',
-    providerId: 'google'
-    },
-    {
-      id: 'gemini-2.0-flash-thinking-exp-01-21',
-      name: 'PATOOPACE ASSIST',
-      provider: 'Create human-quality text',
-      providerId: 'google'
-      }
+    name: '99PACE',
+     provider: 'Speed and Efficiency: Prioritizes fast response times and low latency, it ideal for TASK',
+     providerId: 'google'
+    }
 ]
