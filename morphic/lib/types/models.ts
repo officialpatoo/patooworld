3046d5336753f2e619361667506449b6cca265d8
PATOOWORLD <favour120012@icloud.com>
2025-01-23 14:55:50 -0800

    Update models.ts
    TEST NEW MODEL

--- morphic/lib/types/models.ts
+++ morphic/lib/types/models.ts
@@ -6,10 +6,22 @@ export interface Model {
 }
 
 export const models: Model[] = [
- {
+{
   id: 'gemini-1.5-pro-002',
-  name: 'PATOOWORLD',
-  provider: '',
+  name: 'PATOO ASSIST',
+  provider: 'Create human-quality text, write different kinds of creative content',
   providerId: 'google'
-  }
+  },
+  {
+    id: 'gemini-2.0-flash-exp',
+    name: '99PACE AI',
+     provider: 'Speed and Efficiency: Prioritizes fast response times and low latency, making it ideal for time-sensitive and high-frequency tasks',
+    providerId: 'google'
+    },
+    {
+      id: 'gemini-2.0-flash-thinking-exp-01-21',
+      name: 'PATOOPACE ASSIST',
+      provider: 'Create human-quality text',
+      providerId: 'google'
+      }
 ]
