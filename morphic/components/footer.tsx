8f9d82890b353e193791d13e4480e04bb989900a
PATOOWORLD <favour120012@icloud.com>
2025-01-27 18:32:41 -0800

    Update footer.tsx
    removed discord. add youtube feature

--- morphic/components/footer.tsx
+++ morphic/components/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
+import { SiGithub, SiYoutube } from 'react-icons/si'
+import { FaLinkedin, FaFacebook } from 'react-icons/fa'
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
@@ -12,8 +13,8 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
+          <Link href="https://www.youtube.com/@PATOOWORLD" target="_blank">
+            <SiYoutube size={18} />
           </Link>
         </Button>
         <Button
@@ -21,25 +22,24 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50 mx-2" 
         >
-          <Link href="https://x.com/patooworld" target="_blank">
-            <SiX size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
           <Link href="https://github.com/patooworld" target="_blank">
             <SiGithub size={18} />
           </Link>
         </Button>
       </div>
       <div className="text-xs text-muted-foreground mt-2"> 
-        {/* Added a new div for the copyright information */}
-        GEMINI 1.5 PRO and 2.0 POWERED. 
-        <Link href="https://linkedin.com/company/patooworld" target="_blank">
-          <a className="hover:text-primary">LINKEDIN</a>
+        GEMINI 1.5 Pro and 2.0 POWERED. 
+        <Link href="https://www.linkedin.com/company/patooworld/" target="_blank">
+          <a className="flex items-center hover:text-primary">
+            <FaLinkedin className="mr-1" /> 
+            LinkedIn
+          </a>
+        </Link>
+        <Link href="https://www.facebook.com/patooworld/" target="_blank">
+          <a className="flex items-center ml-2 hover:text-primary"> 
+            <FaFacebook className="mr-1" /> 
+            Facebook
+          </a>
         </Link>
       </div>
     </footer>
