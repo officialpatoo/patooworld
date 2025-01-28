327dbd97452c4f6790df68a38c9f444a7f8260ba
PATOOWORLD <favour120012@icloud.com>
2025-01-27 16:13:57 -0800

    Update models.ts
    update functionality

--- morphic/lib/types/models.ts
+++ morphic/lib/types/models.ts
@@ -9,13 +9,19 @@ export const models: Model[] = [
 {
   name: 'PATOOWORLD AI',
   id: 'gemini-1.5-pro-002',
-  provider: 'Gemini 1.5 and 2.0 Powered',
+  provider: 'Core Functionality: The AI Assist is based on user’s choice and for all purpose',
   providerId: 'google'
   },
   {
     name: '99PACE AG',
      id: 'gemini-2.0-flash-exp',
-     provider: '',
+     provider: 'Speed and Efficiency text, images, chat, imagination image generation, search engine, and SEO',
+     providerId: 'google'
+    },
+   {
+    name: '',
+     id: 'gemini-2.0-flash',
+     provider: 'Gemini 1.5 and 2.0 Powered',
      providerId: 'google'
     }
 ]
