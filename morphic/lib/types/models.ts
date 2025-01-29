512666920f0856154d57c81e34f978ad6c4b0f2a
PATOOWORLD <favour120012@icloud.com>
2025-01-29 14:14:28 -0800

    models.ts

--- morphic/lib/types/models.ts
+++ morphic/lib/types/models.ts
@@ -7,15 +7,15 @@ export interface Model {
 
 export const models: Model[] = [
 {
-  name: 'PATOOWORLD AI',
   id: 'gemini-1.5-pro-002',
+  name: 'PATOOWORLD AI',
   provider: 'Functionality: The AI Assist is based on user’s choice as the AI continues Evolving for all purpose.',
   providerId: 'google'
   },
   {
-    name: '99PACE AG',
-     id: 'gemini-2.0-flash-exp',
-     provider: 'completing tasks, engaging in conversations, creative content, translate languages, and summarize text.',
-     providerId: 'google'
-    }
+  id: 'gemini-2.0-flash-exp',
+  name: '99PACE AG',
+  provider: 'completing tasks, engaging in conversations, creative content, translate languages, and summarize text.',
+  providerId: 'google'
+  }
 ]
