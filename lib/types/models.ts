2d1845b4cf6587a1de754b32ffcb7af39caa0ff3
PATOOWORLD <support@patooworld.com>
2025-01-29 22:53:41 -0500

    Update models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -9,13 +9,13 @@ export const models: Model[] = [
 {
   id: 'gemini-1.5-pro-002',
   name: 'PATOOWORLD AI',
-  provider: 'Google Generative AI',
+  provider: 'PATOOWORLD',
   providerId: 'google'
   },
   {
   id: 'gemini-2.0-flash-exp',
   name: '99PACE AG',
-  provider: 'Google Generative AI',
+  provider: 'PATOOWORLD',
   providerId: 'google'
   }
 ]
