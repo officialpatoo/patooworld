81e8732009229eff1e70594a116d4ea531039715
PATOOWORLD <support@patooworld.com>
2025-01-31 11:15:15 -0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
-import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
+import { SiLinkedin, SiGithub, SiX } from 'react-icons/si'
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
+        <Button
+          variant={'ghost'}
+          size={'icon'}
+          className="text-muted-foreground/50"
+        >
+          <Link href="https://linkedin.com/in/patooworld" target="_blank">
+            <SiLinkedin size={18} />
+          </Link>
+        </Button>
         <Button
           variant={'ghost'}
           size={'icon'}
